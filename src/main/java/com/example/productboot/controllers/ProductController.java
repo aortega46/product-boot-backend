729251670f2554fb6b8766f3a8376b9041cbec8b
Albert Ortega <trebla.og46@gmail.com>
2024-04-20 17:30:40 +0200

    feat: :passport_control: cross origin

--- src/main/java/com/example/productboot/controllers/ProductController.java
+++ src/main/java/com/example/productboot/controllers/ProductController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.example.productboot.services.IProductService;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin
 public class ProductController {
 
   @Autowired
